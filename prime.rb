0ad929637c010bb9dbb50b95cd10e7637ec4e30c
Kevin W <kjoewill@gmail.com>
2018-08-22 17:57:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,23 @@
+function is_prime(n)
+     if n ≤ 1
+        return false
+     else if n ≤ 3
+        return true
+     else if n mod 2 = 0 or n mod 3 = 0
+        return false
+     let i ← 5
+     while i * i ≤ n
+        if n mod i = 0 or n mod (i + 2) = 0
+            return false
+        i ← i + 6
+     return true
+
+
+
 def prime?(i)
+  if i <= 1
+    return false
+  elsif i <= 3
+    return true
+    
 end
